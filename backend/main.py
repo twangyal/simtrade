0558f8c6efac15d0e351e0903adb57417bd248ed
twangyal <tsering.w1029@gmail.com>
2024-03-05 21:41:15 -0800

    added frontend, working on buying/selling function

--- backend/main.py
+++ backend/main.py
@@ -1,12 +1,14 @@
 import numpy as np
 import requests
 import json
+import datetime
+
+from models import Trade
+from flask import request, jsonify
+from config import app, db
 
 
-url = "https://api.coindesk.com/v1/bpi/currentprice.json"
-response = requests.get(url).json()
-
-buyingPower = 25000
+"""buyingPower = 25000
 portfolio = {
     'BTC':.5,
 }
@@ -17,11 +19,41 @@ ticker = {
 totalValue = 0
 commission = 0.002
 print(type(ticker["BTC"]))
-print(response['time']['updated'])
+print(response['time']['updated'])"""
+
+#Get a database of all trades made
+@app.route("/trades",methods=["GET"])
+def get_trades():
+    trades = Trade.query.all()
+    json_trades = list(map(lambda x: x.to_json(), trades))
+    return jsonify({"trades": json_trades}),200
+
+#Make a trade
+@app.route("/create_trade", methods=["POST"])
+def create_trade():
+    buy_or_sell = request.json.get("buyOrSell")
+    instrument = request.json.get("instrument")
+    time = int(datetime.now())
+
+    if not buy_or_sell or not instrument:
+        return(
+            jsonify({"message": "You must include an instrument"}),
+            400,
+        )
+
+    new_trade = Trade(instrument=instrument,buy_or_sell=buy_or_sell,time=time)
+    try:
+        db.session.add(new_trade)
+        db.session.commit()
+    except Exception as e:
+        return jsonify({"message":str(e)}),400
+    return jsonify({"message":"Trade Executed!"}),201
 
 
 
-def calculateTotalValue():
+
+
+"""def calculateTotalValue():
     my_shares = np.array([portfolio[key] for key in sorted(portfolio.keys())])
     prices = np.array([ticker[key] for key in sorted(ticker.keys())])
     return buyingPower+np.sum(np.multiply(my_shares, prices))
@@ -50,4 +82,4 @@ def sell(n):
     totalValue=calculateTotalValue()
     print(totalValue)
 
-sell(.5)
+sell(.5)"""
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import tradeList from './tradeList'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [trades, setTrades] = useState([])
+  const [email, setEmail] = useState(existingContact.email||"")
 
+  useEffect(() => {
+    fetchTrades()
+  }, [])
+  const fetchTrades = async () =>{
+    const response = await fetch("http://127.0.0.1:5000/trades", "GET")
+    const data = await response.json()
+    setTrades(data.trades)
+    console.log(data.trades)
+  }
+  /**const tradeLong = async () =>{
+    const response = await fetch("http://127.0.0.1:5000/trades", "POST")
+    if(response.status !== 201&& response.status !== 200){
+      const data = await response.json()
+      alert(data.message)
+  }else{
+      updateCallback()
+  }
+  }**/
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <tradeList trades = {trades}/>
+      <button onClick={openCreateModal}>Create new order</button>
     </>
   )
 }
